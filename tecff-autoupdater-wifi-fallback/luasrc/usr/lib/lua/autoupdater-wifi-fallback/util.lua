d7f19ac731cfbec33632310b988e269205787fa5
Karsten Böddeker <freifunk@kb-light.de>
2017-06-24 13:17:00 +0200

    autoupdater-wifi-fallback: update + code improvements

--- tecff-autoupdater-wifi-fallback/luasrc/usr/lib/lua/autoupdater-wifi-fallback/util.lua
+++ tecff-autoupdater-wifi-fallback/luasrc/usr/lib/lua/autoupdater-wifi-fallback/util.lua
@@ -1,27 +1,26 @@
 #!/usr/bin/lua
 local uci = require('simple-uci').cursor()
-local iwinfo = require "iwinfo"
+local iwinfo = require 'iwinfo'
 
 function get_available_wifi_networks()
   local radios = {}
 
   uci:foreach('wireless', 'wifi-device',
     function(s)
-	  radio[s['.name']] = {}
+      radios[s['.name']] = {}
     end
   )
 
   for radio, _ in pairs(radios) do
     local wifitype = iwinfo.type(radio)
     local iw = iwinfo[wifitype]
-    if iw then
-      local tmplist = iw.scanlist(radio)
-      for _, net in ipairs(tmplist) do
-        if net.ssid and net.bssid then
-          if net.ssid:match('.*[Ff][Rr][Ee][Ii][Ff][Uu][Nn][Kk].*') then
-            table.insert (radios[radio], net)
-          end
-        end
+    if not iw then
+      return null
+	end
+    local tmplist = iw.scanlist(radio)
+    for _, net in ipairs(tmplist) do
+      if net.ssid and net.bssid and net.ssid:match('.*[Ff][Rr][Ee][Ii][Ff][Uu][Nn][Kk].*') then
+        table.insert (radios[radio], net)
       end
     end
   end
@@ -34,7 +33,7 @@ function get_update_hosts(branch)
   local mirrors = uci:get_list('autoupdater', branch, 'mirror')
 
   for _, mirror in ipairs(mirrors) do
-    local host = mirror:match("://%[?([a-zA-Z0-9\:\.]+)%]?/")
+    local host = mirror:match('://%[?([a-zA-Z0-9\:\.]+)%]?/')
     table.insert(hosts, 1, host)
   end
   return hosts
