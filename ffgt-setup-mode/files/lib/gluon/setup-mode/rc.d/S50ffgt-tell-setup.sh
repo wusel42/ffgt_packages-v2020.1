42429709b5b25c38de9387d3b83bcd9c73897fac
wusel42 <wusel+src@uu.org>
2018-07-19 00:52:57 +0200

    Many changes ...

--- ffgt-setup-mode/files/lib/gluon/setup-mode/rc.d/S50ffgt-tell-setup.sh
+++ ffgt-setup-mode/files/lib/gluon/setup-mode/rc.d/S50ffgt-tell-setup.sh
@@ -6,3 +6,5 @@ START=50
 # Run every 15 seconds ...
 /lib/gluon/config-mode/notify-setup.sh force
 ((sleep 15 ; /lib/gluon/setup-mode/rc.d/S50ffgt-tell-setup.sh force)&)
+# Reboot if in setup mode for more that 3600 seconds (i. e. 1 hour) -- accidental invocation?
+/usr/bin/awk < /proc/uptime '{if($1 > 3600.0) {printf("/sbin/reboot\n");} else {printf("echo -n\n");}}'| /bin/sh
---
+++ ffgt-setup-mode/files/lib/gluon/upgrade/001-ffgt-gluon-hacks.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Another big fat ugly hack ...
+# Rationale: we don't want to change too much on Gluons core files,
+# but we need to replace some for practical reasons. Thus we clone
+# the original, stash them in the firmware as -ffgt versions and
+# on first boot replace the Gluon ones with their FFGT counterparts.
+# Yes, what we would need is some kind of overlay packages that may
+# replace existig files during image creation.
+#
+# Hmm, looks like it's possible to have Gluon apply patches to itself?
+# Anyone daring to explain that magic to me? -- wusel, 2018-07-19 FIXME!
+
+START=1
+
+#if [ -e /lib/gluon/setup-mode/rc.d/S20network ]; then
+# /bin/echo -e "#!/bin/sh\nexit 0\n" >/lib/gluon/setup-mode/rc.d/S20network
+#fi
+
+#if [ -e /lib/gluon/setup-mode/rc.d/S60dnsmasq  ]; then
+# /bin/echo -e "#!/bin/sh\nexit 0\n" >/lib/gluon/setup-mode/rc.d/S60dnsmasq
+#fi
+
+#if [ -e /usr/lib/lua/gluon/util-ffgt.lua ]; then
+#  /bin/mv /usr/lib/lua/gluon/util-ffgt.lua /usr/lib/lua/gluon/util.lua
+#fi
+
+#if [ -e /etc/config/siteselect.upgrade ]; then
+#  mv /etc/config/siteselect.upgrade /etc/config/siteselect
+#fi
+
+#if [ -e /lib/gluon/upgrade/320-setup-ifname-ffgt ]; then
+#  mv /lib/gluon/upgrade/320-setup-ifname-ffgt /lib/gluon/upgrade/320-setup-ifname
+#fi
+
+if [ -e /lib/gluon/config-mode/wizard/0400-geo-location-ffgt.lua ]; then
+  mv /lib/gluon/config-mode/wizard/0400-geo-location-ffgt.lua /lib/gluon/config-mode/wizard/0400-geo-location.lua
+fi
+
+if [ -e /lib/gluon/config-mode/wizard/0200-domain-select-ffgt.lua ]; then
+  mv /lib/gluon/config-mode/wizard/0200-domain-select-ffgt.lua /lib/gluon/config-mode/wizard/0200-domain-select.lua
+fi
