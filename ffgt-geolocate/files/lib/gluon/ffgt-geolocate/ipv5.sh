d9693594a86dd551e97fbddc8f97785f2c29fb0c
wusel42 <wusel+src@uu.org>
2018-08-22 01:40:27 +0200

    Fixes

--- ffgt-geolocate/files/lib/gluon/ffgt-geolocate/ipv5.sh
+++ ffgt-geolocate/files/lib/gluon/ffgt-geolocate/ipv5.sh
@@ -4,6 +4,8 @@
 # Bloody v4/v6 issues ... From an IPv4-only upstream, the preferred IPv6 AAAA record results in connection errors.
 # 2016-09-28: Argh. When in normal mode, resolving via (IPv4) upstream only works as group gluon-fastd. So
 # we (mis-) use /sbin/start-stop-daemon for executing ping as that gid.
+# 2018-08-18: Dropped that shit for v2018; no uplink to mesh, no connectivity, no care.
+if [ -e /tmp/is_online ]; then /bin/rm /tmp/is_online ; fi
 USEIPV4=1
 USEIPV6=0
 /bin/ping -q -c 3 setup.ipv4.4830.org >/dev/null 2>&1
@@ -12,12 +14,6 @@ if [ $? -ne 0 ]; then
  /bin/ping -q -c 3 setup.ipv6.4830.org >/dev/null 2>&1
  if [ $? -eq 0 ]; then
   USEIPV6=1
- else
-  # Try v6 via mesh DNS
-  /bin/ping -q -c 3 setup.ipv6.4830.org >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-   USEIPV6=1
-  fi
  fi
 fi
 IPVXPREFIX="ipv6."
@@ -25,8 +21,10 @@ if [ $USEIPV4 -eq 1 ]; then
  IPVXPREFIX="ipv4."
 fi
 if [ $USEIPV4 -eq 0 -a $USEIPV6 -eq 0 ]; then
- #echo "$0: IPv5 not implemented."
+ echo "$0: IPv5 not implemented." >/dev/stderr
  IPVXPREFIX="ipv5."
+ else
+ echo "online with ${IPVXPREFIX}" >/tmp/is_online
 fi
 
 echo $IPVXPREFIX
