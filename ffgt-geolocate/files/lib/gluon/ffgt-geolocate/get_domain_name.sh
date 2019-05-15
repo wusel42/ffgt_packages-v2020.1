34ff630596a9883d43b4ed0570614959fa3ccc02
wusel42 <wusel+src@uu.org>
2019-05-15 19:01:11 +0200

    Refactor parsing (get rid of superfluous "brokers")

--- ffgt-geolocate/files/lib/gluon/ffgt-geolocate/get_domain_name.sh
+++ ffgt-geolocate/files/lib/gluon/ffgt-geolocate/get_domain_name.sh
@@ -3,5 +3,9 @@
 if [ $# -ne 1 ]; then
  exit 1
 fi
+locode="$1"
+if [ ! -e /lib/gluon/domains/${locode}.json ]; then
+ locode="zzz"
+fi
 
-/usr/bin/jsonfilter -e '@.*' </lib/gluon/domains/${1}.json | /bin/grep "${1}" | /bin/sed -e 's/{ //g' -e 's/ }//g' -e 's/\"//g' -e 's/: /:/g' | cut -d : -f 2
+/usr/bin/jsonfilter -e '@.domain_names' </lib/gluon/domains/${locode}.json | /bin/grep "${1}" | /bin/sed -e 's/{ //g' -e 's/ }//g' -e 's/\"//g' -e 's/: /:/g' | cut -d : -f 2
