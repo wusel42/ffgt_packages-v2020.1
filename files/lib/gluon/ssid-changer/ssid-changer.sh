57760ddcd248532b7370b08d627652382d1f5c23
Andreas Ziegler <dev@andreas-ziegler.de>
2020-09-12 15:42:38 +0200

    ssid-changer: use quoting of variables everywhere

--- files/lib/gluon/ssid-changer/ssid-changer.sh
+++ files/lib/gluon/ssid-changer/ssid-changer.sh
@@ -32,7 +32,7 @@ LOWER_LIMIT='15' # below this limit the offline SSID will be used
 
 # generate an Offline SSID with the first and last part of the node's name to be able to recognise which node is down
 NODENAME="$(uname -n)"
-if [ ${#NODENAME} -gt $((30 - ${#OFFLINE_PREFIX})) ] ; then
+if [ "${#NODENAME}" -gt "$((30 - ${#OFFLINE_PREFIX}))" ] ; then
 	HALF="$(( (28 - ${#OFFLINE_PREFIX} ) / 2 ))" # calculate the length of the first part of the node identifier in the offline-ssid
 	SKIP="$(( ${#NODENAME} - $HALF ))" # jump to this character for the last part of the name
 	OFFLINE_SSID="$OFFLINE_PREFIX${NODENAME:0:$HALF}...${NODENAME:$SKIP:${#NODENAME}}" # use the first and last part of the nodename for nodes with long names
@@ -43,21 +43,22 @@ fi
 # check for an active gateway and get its connection quality (TQ)
 GATEWAY_TQ="$(batctl gwl | grep -e "^=>" -e "^\*" | awk -F'[()]' '{print $2}'| tr -d " ")"
 
-# set TQ to zero if there's no gateway
+# initialize empty variables
 [ -n "$GATEWAY_TQ" ] || GATEWAY_TQ=0
+[ -n "$HUP_NEEDED" ] || HUP_NEEDED=0
 
-if [ $GATEWAY_TQ -gt $UPPER_LIMIT ];
+if [ "$GATEWAY_TQ" -gt "$UPPER_LIMIT" ];
 then
 	logger -s -t "$SCRIPTNAME" -p 5 "gateway TQ is $GATEWAY_TQ, node is online"
 	for HOSTAPD in $(ls /var/run/hostapd-phy*); do # check status of all physical WLAN devices
 		CURRENT_SSID="$(grep "^ssid=$ONLINE_SSID" $HOSTAPD | cut -d"=" -f2)"
-		if [ $CURRENT_SSID == $ONLINE_SSID ]
+		if [ "$CURRENT_SSID" == "$ONLINE_SSID" ]
 		then
 			HUP_NEEDED=0
 			continue
 		fi
 		CURRENT_SSID="$(grep "^ssid=$OFFLINE_SSID" $HOSTAPD | cut -d"=" -f2)"
-		if [ $CURRENT_SSID == $OFFLINE_SSID ]
+		if [ "$CURRENT_SSID" == "$OFFLINE_SSID" ]
 		then
 			logger -s -t "$SCRIPTNAME" -p 5 "TQ is $GATEWAY_TQ, SSID is $CURRENT_SSID, changing to $ONLINE_SSID"
 			sed -i "s/^ssid=$CURRENT_SSID/ssid=$ONLINE_SSID/" $HOSTAPD
@@ -68,18 +69,18 @@ then
 	done
 fi
 
-if [ $GATEWAY_TQ -lt $LOWER_LIMIT ];
+if [ "$GATEWAY_TQ" -lt "$LOWER_LIMIT" ];
 then
 	logger -s -t "$SCRIPTNAME" -p 5 "gateway TQ is $GATEWAY_TQ, node is considered offline"
 	for HOSTAPD in $(ls /var/run/hostapd-phy*); do # check status of all physical WLAN devices
 		CURRENT_SSID="$(grep "^ssid=$OFFLINE_SSID" $HOSTAPD | cut -d"=" -f2)"
-		if [ $CURRENT_SSID == $OFFLINE_SSID ]
+		if [ "$CURRENT_SSID" == "$OFFLINE_SSID" ]
 		then
 			HUP_NEEDED=0
 			continue
 		fi
 		CURRENT_SSID="$(grep "^ssid=$ONLINE_SSID" $HOSTAPD | cut -d"=" -f2)"
-		if [ $CURRENT_SSID == $ONLINE_SSID ]
+		if [ "$CURRENT_SSID" == "$ONLINE_SSID" ]
 		then
 			logger -s -t "$SCRIPTNAME" -p 5 "TQ is $GATEWAY_TQ, SSID is $CURRENT_SSID, changing to $OFFLINE_SSID"
 			sed -i "s/^ssid=$ONLINE_SSID/ssid=$OFFLINE_SSID/" $HOSTAPD
@@ -91,12 +92,12 @@ then
 fi
 
 # don't do anything if the TQ is between the two thresholds
-if [ $GATEWAY_TQ -ge $LOWER_LIMIT -a $GATEWAY_TQ -le $UPPER_LIMIT ]; then
+if [ "$GATEWAY_TQ" -ge "$LOWER_LIMIT" -a "$GATEWAY_TQ" -le "$UPPER_LIMIT" ]; then
 	logger -s -t "$SCRIPTNAME" -p 5 "TQ $GATEWAY_TQ is between the the lower&upper limits, doing nothing"
 	HUP_NEEDED=0
 fi
 
-if [ $HUP_NEEDED == 1 ]; then
+if [ "$HUP_NEEDED" == "1" ]; then
 	killall -HUP hostapd # sending HUP signal to all hostapd processes in order to use the changed SSID
 	HUP_NEEDED=0
 	logger -s -t "$SCRIPTNAME" -p 5 "reloading hostapd with HUP"
