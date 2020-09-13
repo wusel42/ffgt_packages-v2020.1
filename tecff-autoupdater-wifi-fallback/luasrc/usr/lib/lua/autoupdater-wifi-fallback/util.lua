e90e1b78d1c3ce0cc1278af387d93f0edb6e03bc
Andreas Ziegler <dev@andreas-ziegler.de>
2020-09-13 02:22:01 +0200

    autoupdater-wifi-fallback: change indention to tabs

--- tecff-autoupdater-wifi-fallback/luasrc/usr/lib/lua/autoupdater-wifi-fallback/util.lua
+++ tecff-autoupdater-wifi-fallback/luasrc/usr/lib/lua/autoupdater-wifi-fallback/util.lua
@@ -3,38 +3,38 @@ local uci = require('simple-uci').cursor()
 local iwinfo = require 'iwinfo'
 
 function get_available_wifi_networks()
-  local radios = {}
+	local radios = {}
 
-  uci:foreach('wireless', 'wifi-device',
-    function(s)
-      radios[s['.name']] = {}
-    end
-  )
+	uci:foreach('wireless', 'wifi-device',
+		function(s)
+			radios[s['.name']] = {}
+		end
+	)
 
-  for radio, _ in pairs(radios) do
-    local wifitype = iwinfo.type(radio)
-    local iw = iwinfo[wifitype]
-    if not iw then
-      return null
+	for radio, _ in pairs(radios) do
+		local wifitype = iwinfo.type(radio)
+		local iw = iwinfo[wifitype]
+		if not iw then
+			return null
+		end
+		local tmplist = iw.scanlist(radio)
+		for _, net in ipairs(tmplist) do
+			if net.ssid and net.bssid and net.ssid:match('.*[Ff][Rr][Ee][Ii][Ff][Uu][Nn][Kk].*') then
+				table.insert (radios[radio], net)
+			end
+		end
 	end
-    local tmplist = iw.scanlist(radio)
-    for _, net in ipairs(tmplist) do
-      if net.ssid and net.bssid and net.ssid:match('.*[Ff][Rr][Ee][Ii][Ff][Uu][Nn][Kk].*') then
-        table.insert (radios[radio], net)
-      end
-    end
-  end
 
-  return radios
+	return radios
 end
 
 function get_update_hosts(branch)
-  local hosts = {}
-  local mirrors = uci:get_list('autoupdater', branch, 'mirror')
+	local hosts = {}
+	local mirrors = uci:get_list('autoupdater', branch, 'mirror')
 
-  for _, mirror in ipairs(mirrors) do
-    local host = mirror:match('://%[?([a-zA-Z0-9\:\.]+)%]?/')
-    table.insert(hosts, 1, host)
-  end
-  return hosts
+	for _, mirror in ipairs(mirrors) do
+		local host = mirror:match('://%[?([a-zA-Z0-9\:\.]+)%]?/')
+		table.insert(hosts, 1, host)
+	end
+	return hosts
 end
