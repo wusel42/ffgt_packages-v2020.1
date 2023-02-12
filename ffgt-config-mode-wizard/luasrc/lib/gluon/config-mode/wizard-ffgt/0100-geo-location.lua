51da902a86ef37ff8bbe17e1e2761b57e0aa4d50
wusel <wusel+src@uu.org>
2023-02-12 03:09:03 +0100

    Try ...

--- ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/wizard-ffgt/0100-geo-location.lua
+++ ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/wizard-ffgt/0100-geo-location.lua
@@ -11,22 +11,21 @@ return function(form, uci)
 
 	local show_altitude = site.config_mode.geo_location.show_altitude(false)
 
-	local text = pkg_i18n.translate(
-			"No coordinates set; please add them or try the WiFi-based " ..
-			"geolocation, which will upload the WiFi networks (SSID, " ..
-			"BSSID, strenght, channel) to our server, which in turn will " ..
-			"use third party services (Google, OpenStreetMap, ...) to map " ..
-			that to a location. We *need* a proper location to assign this " ..
-			node to a Freifunk network (‘hood’, ‘community’, ...)."
-	)
-	if osm then
-		text = text .. '\n' .. osm.help(i18n)
-	end
-	if show_altitude then
-		text = text .. '\n ' .. pkg_i18n.translate(
-			'Specifying the altitude is optional; it should only be filled in if an accurate ' ..
-			'value is known.'
+	local text = site_i18n._translate("gluon-config-mode:geo-location-help")
+	if not text then
+		text = pkg_i18n.translate(
+			'If you want the location of your node to ' ..
+			'be displayed on public maps, you can enter its coordinates here.'
 		)
+		if osm then
+			text = text .. ' ' .. osm.help(i18n)
+		end
+		if show_altitude then
+			text = text .. ' ' .. pkg_i18n.translate(
+				'Specifying the altitude is optional; it should only be filled in if an accurate ' ..
+				'value is known.'
+			)
+		end
 	end
 
 	local s = form:section(Section, nil, text)
@@ -36,8 +35,25 @@ return function(form, uci)
 	local own_latitude = uci:get("gluon-node-info", location, "latitude")
 	local own_longitude = uci:get("gluon-node-info", location, "longitude")
 
+	local set_location = s:option(Flag, "location", pkg_i18n.translate("Set node position"))
+	set_location.default = own_latitude or own_longitude
+
+	-- Delete already saved coordinates
+	function set_location:write(data)
+		if not data then
+			uci:delete("gluon-node-info", location, "latitude")
+			uci:delete("gluon-node-info", location, "longitude")
+			uci:set("gluon-node-info", location, "share_location", false)
+		end
+
+		if not show_altitude or not data then
+			uci:delete("gluon-node-info", location, "altitude")
+		end
+	end
+
 	local share_location = s:option(Flag, "share_location", pkg_i18n.translate("Advertise node position"))
 	share_location.default = uci:get_bool("gluon-node-info", location, "share_location")
+	share_location:depends(set_location, true)
 	function share_location:write(data)
 		uci:set("gluon-node-info", location, "share_location", data)
 	end
@@ -45,10 +61,12 @@ return function(form, uci)
 	local map
 	if osm then
 		map = s:option(osm.MapValue, "map", osm.options())
+		map:depends(set_location, true)
 	end
 
 	o = s:option(Value, "latitude", pkg_i18n.translate("Latitude"), pkg_i18n.translatef("e.g. %s", "53.873621"))
 	o.default = own_latitude
+	o:depends(set_location, true)
 	o.datatype = "float"
 	function o:write(data)
 		uci:set("gluon-node-info", location, "latitude", data)
@@ -59,6 +77,7 @@ return function(form, uci)
 
 	o = s:option(Value, "longitude", pkg_i18n.translate("Longitude"), pkg_i18n.translatef("e.g. %s", "10.689901"))
 	o.default = own_longitude
+	o:depends(set_location, true)
 	o.datatype = "float"
 	function o:write(data)
 		uci:set("gluon-node-info", location, "longitude", data)
@@ -73,6 +92,7 @@ return function(form, uci)
 			pkg_i18n.translatef("e.g. %s", "11.51")
 		)
 		o.default = uci:get("gluon-node-info", location, "altitude")
+		o:depends(set_location, true)
 		o.datatype = "float"
 		o.optional = true
 		function o:write(data)
