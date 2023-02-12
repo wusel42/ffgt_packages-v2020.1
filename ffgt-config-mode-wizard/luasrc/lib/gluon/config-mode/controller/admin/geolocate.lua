95994efb085d44b6f9f4baa08af5323b4bb974d0
wusel <wusel+src@uu.org>
2023-02-12 04:36:13 +0100

    fixes

--- ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/controller/admin/geolocate.lua
+++ ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/controller/admin/geolocate.lua
@@ -9,7 +9,7 @@ You may obtain a copy of the License at
 	http://www.apache.org/licenses/LICENSE-2.0
 ]]--
 
-package 'ffgt-geolocate'
+package 'ffgt-config-mode-wizard'
 
 local util = require 'gluon.util'
 local site = require 'gluon.site'
