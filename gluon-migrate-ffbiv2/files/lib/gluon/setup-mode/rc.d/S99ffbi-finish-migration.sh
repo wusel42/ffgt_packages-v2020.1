5d339f0a3bf24fcdc46758e586b1dc99f5d85fd7
wusel <wusel+src@uu.org>
2023-02-23 23:49:23 +0100

    Refactor

--- gluon-migrate-ffbiv2/files/lib/gluon/setup-mode/rc.d/S99ffbi-finish-migration.sh
+++ gluon-migrate-ffbiv2/files/lib/gluon/setup-mode/rc.d/S99ffbi-finish-migration.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+#/etc/rc.common
+
+START=99
 
 if [ -x /tmp/ffbi-migration.sh ]; then
   echo "Running /tmp/ffbi-migration.sh at $(date) ..." | tee -a /root/ffbi-migration.log
   mv /tmp/ffbi-migration.sh /tmp/ffbi-migration-running.sh
-  ((/tmp/ffbi-migration-running.sh)&) ||:
+  ((sleep 15 ; /tmp/ffbi-migration-running.sh)&) ||:
 fi
