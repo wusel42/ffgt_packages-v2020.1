7e9ae6bcad2d63d3ca73918ce75b6a70b3892c83
wusel <wusel+src@uu.org>
2023-02-25 00:30:17 +0100

    Fix

--- gluon-migrate-ffbiv2/files/lib/gluon/setup-mode/rc.d/S99ffbi-finish-migration.sh
+++ gluon-migrate-ffbiv2/files/lib/gluon/setup-mode/rc.d/S99ffbi-finish-migration.sh
@@ -83,41 +83,9 @@ if [ -e /etc/config/freifunk ]; then
   ((sleep 50 ; (date ; /lib/gluon/ffgt-geolocate/rgeo.sh 2>&1) | tee -a /root/ffbi-migration.log)&)
 
   echo "$0: Setting gluon-setup-mode to configured in 72 secs after $(date) ..." | tee -a /root/ffbi-migration.log
-  ((sleep 72 ; (date ; uci set gluon-setup-mode.@setup_mode[0].configured='1' ||: ; uci set gluon-setup-mode.@setup_mode[0].enabled='0' ||: ; uci commit gluon-setup-mode ||:) 2&>1 tee -a /root/ffbi-migration.log)&)
+  ((sleep 72 ; (date ; uci set gluon-setup-mode.@setup_mode[0].configured='1' ||: ; uci set gluon-setup-mode.@setup_mode[0].enabled='0' ||: ; uci commit gluon-setup-mode ||:) 2&>1 | tee -a /root/ffbi-migration.log)&)
 
   echo "$0: Rebooting 75 secs after $(date) ..." | tee -a /root/ffbi-migration.log
   sync
   ((sleep 75 ; sync; sync; sync; sleep 1 ; /sbin/reboot)&)
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-if [ -x /tmp/ffbi-migration.sh ]; then
-  mv /tmp/ffbi-migration.sh /tmp/ffbi-migration-running.sh
-  echo "uci set gluon-setup-mode.@setup_mode[0].configured='1' ||:"     >>/tmp/ffbi-migration-running.sh
-  echo "uci set gluon-setup-mode.@setup_mode[0].enabled='0' ||:"        >>/tmp/ffbi-migration-running.sh
-  echo "uci commit gluon-setup-mode ||:"                                >>/tmp/ffbi-migration-running.sh
-
-  echo "$0: Running /tmp/ffbi-migration-running.sh 10 secs after $(date) ..." | tee -a /root/ffbi-migration.log
-  ((sleep 10 ; date ; /bin/sh -x /tmp/ffbi-migration-running.sh 2&>1 | tee -a /root/ffbi-migration.log)&) ||:
-  cp -p /tmp/ffbi-migration-running.sh /root/ ||:
-
-  echo "$0: Running rgeo.sh 20 secs after $(date) ..." | tee -a /root/ffbi-migration.log
-  ((sleep 20 ; date ; /lib/gluon/ffgt-geolocate/rgeo.sh 2>&1 | tee -a /root/ffbi-migration.log)&)
-
-  echo "$0: Running gluon-reconfigure 30 secs after $(date) ..." | tee -a /root/ffbi-migration.log
-  ((sleep 30 ; date ; gluon-reconfigure 2>&1 | tee -a /root/ffbi-migration.log)&)
-
-  echo "$0: Rebooting 45 secs after $(date) ..." | tee -a /root/ffbi-migration.log
-  sync
-  ((sleep 45 ; /sbin/reboot)&)
 fi
