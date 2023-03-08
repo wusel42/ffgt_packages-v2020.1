d305f60bb9b8fc5300f4d14407e56527b36ab352
wusel <wusel+src@uu.org>
2023-03-09 00:44:43 +0100

    Fixes

--- ffgt-geolocate/luasrc/lib/gluon/config-mode/wizard/0090-ssh-keys.lua
+++ ffgt-geolocate/luasrc/lib/gluon/config-mode/wizard/0090-ssh-keys.lua
@@ -8,16 +8,17 @@ return function(form, uci)
 
 	local text=""
 	local text2=admin_i18n.translate("Remote access")
-	local cmdstr='wc -l /etc/dropbear/authorized_keys 2>/dev/null || echo 0'
+	local cmdstr='(wc -l /etc/dropbear/authorized_keys 2>/dev/null || echo 0)| cut -d " " -f 1'
 	rc=util.trim(util.exec(cmdstr))
 	if rc=="0" then
 		text = ffgt_i18n.translate("WARNING: You have not configured any SSH keys, you will not have SSH access to the running node!")
+		text = "<strong>" .. text .. "</strong>"
 	else
-		text = ffgt_i18n.translate('There are %s SSH keys configured, fine!')
+		text = ffgt_i18n.translate('There are SSH keys (%s) configured, fine!')
 		text = string.format(text, rc)
 	end
-	text = text .. "<br>&nbsp;<br>"
-	text = text .. pkg_i18n.translate('To change it, go to %s.')
+	text = text .. "<br> </br><br> </br>"
+	text = text .. ffgt_i18n.translate('To change it, go to %s.')
 	text = string.format(text, '<a href="/cgi-bin/config/admin/remote">%s</a>')
 	text = string.format(text, text2)
 	local s = form:section(Section, nil, text)
