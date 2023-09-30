58390e3e563aec96e97d08aae321137c162a8dfc
wusel <wusel+src@uu.org>
2023-10-01 00:38:34 +0200

    Fixes

--- ffgt-config-mode-wizard/files/lib/gluon/ffgt-geolocate/rgeo.sh
+++ ffgt-config-mode-wizard/files/lib/gluon/ffgt-geolocate/rgeo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-isconfigured="`$(sbin/uci get gluon-setup-mode.@setup_mode[0].configured 2>/dev/null)"
+isconfigured="$(sbin/uci get gluon-setup-mode.@setup_mode[0].configured 2>/dev/null)"
 if [ "$isconfigured" != "1" ]; then
  isconfigured=0
 fi
@@ -25,20 +25,20 @@ if [ "X${curlat}" != "X" -a "X${curlon}" != "X" ]; then
   # Sanity checks: LAT != 0, locode not "" and known to firmware ...
   grep "LAT: 0$" </tmp/geoloc.out >/dev/null 2>&1
   if [ $? -ne 0 ]; then
-   newsitecode="$(/usr/bin/awk <${DATAFILE} '/^LOC:/ {printf("%s", $2);}')"
+   newsitecode="$(/usr/bin/awk </tmp/geoloc.out '/^LOC:/ {printf("%s", $2);}')"
    if [ "X${newsitecode}" != "X" -a -e /lib/gluon/domains/${newsitecode}.json ]; then
     /bin/cat /dev/null >/tmp/geoloc.sh
     /usr/bin/awk </tmp/geoloc.out '/^LAT:/ {printf("/sbin/uci set gluon-node-info.@location[0].latitude=%s\n", $2);} /^LON:/ {printf("/sbin/uci set gluon-node-info.@location[0].longitude=%s\n", $2);}' >>/tmp/geoloc.sh
     /usr/bin/awk </tmp/geoloc.out '/^ADR:/ {printf("/sbin/uci set gluon-node-info.@location[0].addr=%c%s%c\n", 39, substr($0, 6), 39);} /^CTY:/ {printf("/sbin/uci set gluon-node-info.@location[0].city=%c%s%c\n", 39, substr($0, 6), 39);}' >>/tmp/geoloc.sh
     /usr/bin/awk </tmp/geoloc.out '/^LOC:/ {printf("/sbin/uci set gluon-node-info.@location[0].locode=%s\n", $2)}; /^ZIP:/ {printf("/sbin/uci set gluon-node-info.@location[0].zip=%s\n", $2);}' >>/tmp/geoloc.sh
-    /usr/bin/awk </tmp/geoloc.out '/^LOC:/ END{printf("/sbin/uci commit gluon-node-info\n");}' >>/tmp/geoloc.sh
+    /usr/bin/awk </tmp/geoloc.out 'END{printf("/sbin/uci commit gluon-node-info\n");}' >>/tmp/geoloc.sh
     /bin/sh /tmp/geoloc.sh
     loc="$(/sbin/uci get gluon-node-info.@location[0].locode 2>/dev/null)"
     adr="$(/sbin/uci get gluon-node-info.@location[0].addr 2>/dev/null)"
     zip="$(/sbin/uci get gluon-node-info.@location[0].zip 2>/dev/null)"
     if [ $isconfigured -ne 1 ]; then
       if [ "x${zip}" != "x" -a "x${adr}" != "x" ]; then
-       nodeid=`$(echo "util=require 'gluon.util' print(string.format('%s', string.sub(util.node_id(), 9)))" | /usr/bin/lua)
+       nodeid=$(echo "util=require 'gluon.util' print(string.format('%s', string.sub(util.node_id(), 9)))" | /usr/bin/lua)
        suffix=$(echo "util=require 'gluon.util' print(string.format('%s', string.sub(util.node_id(), 9)))" | /usr/bin/lua)
        hostname="${zip}-${adr}-${suffix}"
        #hostname="${zip}-freifunk-${nodeid}"
