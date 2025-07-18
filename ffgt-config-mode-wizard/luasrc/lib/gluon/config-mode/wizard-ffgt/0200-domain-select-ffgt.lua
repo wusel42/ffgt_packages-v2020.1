587ebd02ee18df1b1bce0d6532ce44c3f0976112
wusel <wusel+src@uu.org>
2025-07-19 00:59:42 +0200

    Refactor

--- ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/wizard-ffgt/0200-domain-select-ffgt.lua
+++ ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/wizard-ffgt/0200-domain-select-ffgt.lua
@@ -1,14 +1,14 @@
 
 return function(form, uci)
-	local site_i18n = i18n 'gluon-site'
-	local ffgt_i18n = i18n 'ffgt-config-mode-wizard'
-	local json = require 'jsonc'
-	local site = require 'gluon.site'
-	local util = require 'gluon.util'
+    local site_i18n = i18n 'gluon-site'
+    local ffgt_i18n = i18n 'ffgt-config-mode-wizard'
+    local json = require 'jsonc'
+    local site = require 'gluon.site'
+    local util = require 'gluon.util'
 
-	local selected_domain = uci:get('gluon', 'core', 'domain')
+    local selected_domain = uci:get('gluon', 'core', 'domain')
     local locode = uci:get_first("gluon-node-info", "location", "locode")
-  	local configured = uci:get_first('gluon-setup-mode','setup_mode', 'configured') == '1' or (selected_domain ~= site.default_domain())
+    local configured = uci:get_first('gluon-setup-mode','setup_mode', 'configured') == '1' or (selected_domain ~= site.default_domain())
 
     if locode then
         -- Selections happens via locode, configuration via domain ...
@@ -16,11 +16,18 @@ return function(form, uci)
             uci:set('gluon', 'core', 'domain', locode)
             os.execute('gluon-reconfigure >/dev/null')
         end
-        local text = ffgt_i18n.translate('Based on the coordinates configured, this node will be part of:')
-        local communityname = string.gsub(util.exec(string.format("/lib/gluon/ffgt-geolocate/get_domain_name.sh %s", locode)),"\n", "")
-        text = text .. " <strong>" .. communityname .. "</strong>."
-     	local s = form:section(Section, nil, text)
-	end
+        local domain = assert(json.load('/lib/gluon/domains/' .. locode .. '.json'))
+        local mesh_name = domain.domain_names[locode] or 'n/a'
+        local community_name = domain.community_name or mesh_name
+        local text = ffgt_i18n.translate('Based on the coordinates configured, this node will be part of')
+        text = text .. " <strong>" .. mesh_name .. "</strong>"
+        if community_name ~= mesh_name then
+            text = text .. ", Community " .. community_name
+        end
+        text = text .. "."
 
-	return {'gluon', reconfigure}
+        local s = form:section(Section, nil, text)
+    end
+
+    return {'gluon', reconfigure}
 end
