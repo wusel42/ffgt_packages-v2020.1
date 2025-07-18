c3ed117b19917dfe47e6aed8b88b1dbc54bb6724
wusel <wusel+src@uu.org>
2025-07-18 19:29:10 +0200

    Updates/Fixes

--- ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/reboot-ffgt/0900-msg-reboot.lua
+++ ffgt-config-mode-wizard/luasrc/lib/gluon/config-mode/reboot-ffgt/0900-msg-reboot.lua
@@ -3,20 +3,28 @@ local site_i18n = i18n 'gluon-site'
 local site = require 'gluon.site'
 local sysconfig = require 'gluon.sysconfig'
 local pretty_hostname = require 'pretty_hostname'
-
+local json = require 'jsonc'
+local util = require 'gluon.util'
 local uci = require("simple-uci").cursor()
 
 local hostname = pretty_hostname.get(uci)
 local contact = uci:get_first('gluon-node-info', 'owner', 'contact')
+local domain_code = uci:get('gluon', 'core', 'domain')
+local domain = assert(json.load('/lib/gluon/domains/' .. domain_code .. '.json'))
+local mesh_name = domain.domain_names[domain_code] or 'n/a'
+local community_name = domain.community_name or mesh_name
+local community_mail = domain.community_contact or 'n/a'
+local community_url = domain.community_website or 'n/a'
 
 local msg = site_i18n._translate('gluon-config-mode:reboot')
 if not msg then return end
-
-msg = msg .. "<p>Thänk you for surfing via 4830.org e. V. ;)<p>"
 
 renderer.render_string(msg, {
 	hostname = hostname,
 	site = site,
 	sysconfig = sysconfig,
 	contact = contact,
+	community_name = community_name,
+	community_mail = community_mail,
+	community_url = community_url
 })
