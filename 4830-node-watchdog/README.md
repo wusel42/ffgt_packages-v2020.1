60c25d6a9b0bc2ff73ed305e92e143eed88cbbe5
wusel <wusel+src@uu.org>
2025-12-22 00:36:40 +0100

    Add 4830-node-watchdog

---
+++ 4830-node-watchdog/README.md
@@ -0,0 +1,7 @@
+4830-node-watchdog
+==================
+
+Script to reboot the node after being offline for more than 135 Minutes.
+
+It is quite basic, it just checks the Quality of the Connection and decides if a reboot might be in order.
+
---
+++ 4830-node-watchdog/files/lib/gluon/4830-node-watchdog/4830-node-watchdog.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+SCRIPTNAME="4830-node-watchdog"
+DEBUG=false
+OFFLINE_FILE="/tmp/${SCRIPTNAME}.offline"
+# 135 Minutes = 8100 Secondes
+MAX_DELTA=8100
+
+# don't do anything while an autoupdater process is running
+pgrep autoupdater >/dev/null
+if [ "$?" == "0" ]; then
+	logger -s -t "$SCRIPTNAME" -p 5 "autoupdater is running, aborting."
+	exit
+fi
+
+# don't run this script if another instance is still running
+exec 201<$0
+flock -n 201
+if [ "$?" != "0" ]; then
+	logger -s -t "$SCRIPTNAME" -p 5 "failed to acquire lock, another instance of this script might still be running, aborting."
+	exit
+fi
+
+# check for an active gateway and get its connection quality (TQ)
+GATEWAY_TQ="$(batctl gwl | grep -e "^=>" -e "^\*" | awk -F'[()]' '{print $2}'| tr -d " ")"
+
+# initialize empty variables
+[ -n "$GATEWAY_TQ" ] || GATEWAY_TQ=0
+
+if [ "$GATEWAY_TQ" -eq "0" ]; then
+    $($DEBUG) && logger -s -t "$SCRIPTNAME" -p 5 "gateway TQ is ${GATEWAY_TQ}, node is considered offline"
+    OFFLINE_SINCE="$(cat ${OFFLINE_FILE} 2>/dev/null)"
+    if [ -n "${OFFLINE_SINCE" ]; then
+        NOW="$(date +%s)"
+        OFFLINE_DELTA="$(expr ${NOW} - ${OFFLINE_SINCE})"
+        if [ "${OFFLINE_DELTA}" -gt "${MAX_DELTA}" ]; then
+            logger -s -t "$SCRIPTNAME" -p 5 "node is considered offline for ${OFFLINE_DELTA} seconds, initiating reboot."
+            sync
+            sleep 2
+            reboot
+        else
+            $($DEBUG) && logger -s -t "$SCRIPTNAME" -p 5 "node is considered offline for ${OFFLINE_DELTA} seconds, waiting until ${MAX_DELTA} ..."
+        fi
+    else
+        date +%s >${OFFLINE_FILE}
+    fi
+else
+    $($DEBUG) && logger -s -t "$SCRIPTNAME" -p 5 "node is considered online, gateway TQ is ${GATEWAY_TQ}."
+    touch ${OFFLINE_FILE} && rm ${OFFLINE_FILE}
+fi
