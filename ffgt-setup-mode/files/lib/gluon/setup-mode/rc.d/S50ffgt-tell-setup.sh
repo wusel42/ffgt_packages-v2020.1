4c4287b8f72b5cf1181b99a15f0a3aff3c023d67
wusel <wusel+src@uu.org>
2026-02-17 04:39:28 +0100

    Add current WAN prefix (br-setup) to iptables to cater for non-1918 environments (e. g. 198.51.100.0/24) ...

--- ffgt-setup-mode/files/lib/gluon/setup-mode/rc.d/S50ffgt-tell-setup.sh
+++ ffgt-setup-mode/files/lib/gluon/setup-mode/rc.d/S50ffgt-tell-setup.sh
@@ -12,3 +12,10 @@ rc=$?
 if [ ${rc} -eq 0 -a "${configured}" == "1" ]; then
  /usr/bin/awk < /proc/uptime '{if($1 > 3600.0) {printf("/sbin/reboot\n");} else {printf("echo -n\n");}}'| /bin/sh
 fi
+
+# Add prefix of WAN interface, i. e. br-setup's LAN, so we can use non-standard
+# LAN setups (like e. g. 198.51.100.0/24 or public v4) as well.
+WANPFX=$(ip -4 route show >/tmp/ip4route.out && awk -v dev=$(awk </tmp/ip4route.out '/^default/ {x=0; for(i=1; i<NF; i++) {if($i=="dev") {x=i+1; i=NF;}} if(x>0) {print $x;}}') '/\// {printf("%s\n", $1);}' </tmp/ip4route.out)
+if [ "${WANPFX}X" != "X" ]; then
+ iptables -n -L INPUT | grep "${WANPFX}" || (iptables -I INPUT -s "${WANPFX}" -p tcp --dport 80 -j ACCEPT ||: ; iptables -I INPUT -s "${WANPFX}" -p tcp --dport 22 -j ACCEPT ||:)
+fi
